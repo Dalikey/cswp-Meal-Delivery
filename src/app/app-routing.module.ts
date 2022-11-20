d5f0c7cc2980341afbfcc32d6913486eb8002583
Junhao Chen <junhao@hotmail.com>
2022-11-20 22:13:33 +0100

    Fix naming

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,39 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from './auth/login/login.component';
-import {RegisterComponent} from './auth/register/register.component';
-import {MealComponent} from './pages/meal/meal.component';
-import {ListComponent as ListComponentMeal} from './pages/meal/list/list.component';
-import {DetailComponent as DetailComponentMeal} from './pages/meal/detail/detail.component';
-import {EditComponent as EditComponentMeal} from './pages/meal/edit/edit.component';
-import {AboutComponent} from './pages/about/about.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {ListComponent as ListComponentUser} from './pages/user/list/list.component';
-import {DetailComponent as DetailComponentUser} from './pages/user/detail/detail.component';
-import {EditComponent as EditComponentUser} from './pages/user/edit/edit.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ListComponent as ListComponentMeal } from './pages/meal/list/list.component';
+import { DetailComponent as DetailComponentMeal } from './pages/meal/detail/detail.component';
+import { EditComponent as EditComponentMeal } from './pages/meal/edit/edit.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ListComponent as ListComponentUser } from './pages/user/list/list.component';
+import { DetailComponent as DetailComponentUser } from './pages/user/detail/detail.component';
+import { EditComponent as EditComponentUser } from './pages/user/edit/edit.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
-  {path: 'dashboard', pathMatch: 'full', component: DashboardComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', pathMatch: 'full', component: DashboardComponent },
 
-  {path: 'meal', pathMatch: 'full', component: ListComponentMeal},
-  {path: 'meal/new', pathMatch: 'full', component: EditComponentMeal},
-  {path: 'meal/:id', pathMatch: 'full', component: DetailComponentMeal},
-  {path: 'meal/:id/edit', pathMatch: 'full', component: EditComponentMeal},
+  { path: 'meal', pathMatch: 'full', component: ListComponentMeal },
+  { path: 'meal/new', pathMatch: 'full', component: EditComponentMeal },
+  { path: 'meal/:id', pathMatch: 'full', component: DetailComponentMeal },
+  { path: 'meal/:id/edit', pathMatch: 'full', component: EditComponentMeal },
 
-  {path: 'about-us', pathMatch: 'full', component: AboutComponent},
+  { path: 'about-us', pathMatch: 'full', component: AboutComponent },
 
-  {path: 'user', pathMatch: 'full', component: ListComponentUser},
-  {path: 'user/new', pathMatch: 'full', component: EditComponentUser},
-  {path: 'user/:id', pathMatch: 'full', component: DetailComponentUser},
-  {path: 'user/:id/edit', pathMatch: 'full', component: EditComponentUser},
+  { path: 'user', pathMatch: 'full', component: ListComponentUser },
+  { path: 'user/new', pathMatch: 'full', component: EditComponentUser },
+  { path: 'user/:id', pathMatch: 'full', component: DetailComponentUser },
+  { path: 'user/:id/edit', pathMatch: 'full', component: EditComponentUser },
 
-  {path: 'register', pathMatch: 'full', component: RegisterComponent},
-  {path: 'login', pathMatch: 'full', component: LoginComponent},
-  {path: '**', redirectTo: 'dashboard'},
+  { path: 'register', pathMatch: 'full', component: RegisterComponent },
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
+  { path: '**', redirectTo: 'dashboard' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
