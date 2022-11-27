7ff866c3b57530532fbae4714b9aff9f6d44b2bd
Junhao Chen <junhao@hotmail.com>
2022-11-27 15:00:26 +0100

    Add auth endpoint

--- apps/meal-delivery-api/src/app/app.module.ts
+++ apps/meal-delivery-api/src/app/app.module.ts
@@ -1,11 +1,39 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
+
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './auth/auth.module';
+import { TokenMiddleware } from './auth/token.middleware';
+import { DataModule } from './data.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      // `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+      'mongodb://127.0.0.1:27017/meal-delivery-api'
+    ),
+    AuthModule,
+    DataModule,
+    RouterModule.register([
+      {
+        path: 'auth-api',
+        module: AuthModule,
+      },
+      {
+        path: 'meal-delivery-api',
+        module: DataModule,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TokenMiddleware).forRoutes('meal-delivery-api');
+  }
+}
---
+++ libs/data/src/lib/user.interface.ts
@@ -0,0 +1,12 @@
+import { Id } from './id.type';
+
+export interface UserIdentity {
+  id: Id;
+  name: string;
+}
+
+export interface UserInfo extends UserIdentity {
+  emailAddress: string;
+}
+
+export interface User extends UserInfo {}
