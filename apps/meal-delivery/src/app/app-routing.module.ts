42339a7758626933c91f3635365240c6561cd5bc
Junhao Chen <junhao@hotmail.com>
2022-12-04 21:21:40 +0100

    Add studentHouse

--- apps/meal-delivery/src/app/app-routing.module.ts
+++ apps/meal-delivery/src/app/app-routing.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MealComponent } from './pages/meal/meal.component';
 import { ListComponent as ListComponentMeal } from './pages/meal/list/list.component';
 import { DetailComponent as DetailComponentMeal } from './pages/meal/detail/detail.component';
 import { EditComponent as EditComponentMeal } from './pages/meal/edit/edit.component';
-import { AboutComponent } from './pages/about/about.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UserComponent } from './pages/user/user.component';
 import { ListComponent as ListComponentUser } from './pages/user/list/list.component';
 import { DetailComponent as DetailComponentUser } from './pages/user/detail/detail.component';
 import { EditComponent as EditComponentUser } from './pages/user/edit/edit.component';
+import { ProductComponent } from './pages/product/product.component';
 import { ListComponent as ListComponentProduct } from './pages/product/list/list.component';
 import { DetailComponent as DetailComponentProduct } from './pages/product/detail/detail.component';
 import { EditComponent as EditComponentProduct } from './pages/product/edit/edit.component';
-import { MealComponent } from './pages/meal/meal.component';
-import { UserComponent } from './pages/user/user.component';
-import { ProductComponent } from './pages/product/product.component';
+import { StudentHouseComponent } from './pages/studentHouse/studentHouse.component';
+import { ListComponent as ListComponentStudentHouse } from './pages/studentHouse/list/list.component';
+import { DetailComponent as DetailComponentStudentHouse } from './pages/studentHouse/detail/detail.component';
+import { EditComponent as EditComponentStudentHouse } from './pages/studentHouse/edit/edit.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
@@ -62,6 +66,20 @@ const routes: Routes = [
         path: ':id/edit',
         pathMatch: 'full',
         component: EditComponentProduct,
+      },
+    ],
+  },
+  {
+    path: 'studentHouse',
+    component: StudentHouseComponent,
+    children: [
+      { path: '', pathMatch: 'full', component: ListComponentStudentHouse },
+      { path: 'new', pathMatch: 'full', component: EditComponentStudentHouse },
+      { path: ':id', pathMatch: 'full', component: DetailComponentStudentHouse },
+      {
+        path: ':id/edit',
+        pathMatch: 'full',
+        component: EditComponentStudentHouse,
       },
     ],
   },
---
+++ apps/meal-delivery/src/app/pages/studentHouse/studentHouse.service.spec.ts
@@ -0,0 +1,77 @@
+import {HttpClient} from '@angular/common/http';
+import {TestBed} from '@angular/core/testing';
+import {StudentHouse} from './studentHouse.model';
+import {StudentHouseService} from './studentHouse.service';
+
+// Global mock objects
+const expectedStudentHouses: StudentHouse[] = [
+  {
+    id: '12345-123-11',
+    streetAndNmr: 'Lovensdijkstraat 61',
+    city: "Breda",
+    postcode: "4818 AJ"
+  },
+];
+
+describe('StudentHouseService', () => {
+  let service: StudentHouseService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{provide: HttpClient}],
+    });
+    service = TestBed.inject(StudentHouseService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return a list of studentHouses', (done: DoneFn) => {
+    const studentHouses = service.getAllStudentHouses();
+    expect(studentHouses.length).toBe(8);
+    expect(studentHouses[0].id).toEqual(expectedStudentHouses[0].id);
+    done();
+  });
+
+  it('should return Lovensdijkstraat 67', (done: DoneFn) => {
+    const studentHouse = service.getStudentHouseById('12345-123-17');
+    expect(studentHouse.streetAndNmr).toEqual('Lovensdijkstraat 67');
+    done();
+  });
+
+  it('should add a studentHouse', (done: DoneFn) => {
+    const newStudentHouse = {
+      id: '12345-123-20',
+      streetAndNmr: 'Lovensdijkstraat 69',
+      city: "Breda",
+      postcode: "4818 AJ"
+    };
+    service.addStudentHouse(newStudentHouse);
+    expect(service.getAllStudentHouses().length).toEqual(9);
+    done();
+  });
+
+  it('should update a studentHouse', (done: DoneFn) => {
+    const newStudentHouse = {
+      id: '12345-123-12',
+      streetAndNmr: 'Lovensdijkstraat 100',
+      city: "Breda",
+      postcode: "4818 AJ"
+    };
+    service.updateStudentHouse(newStudentHouse);
+    expect(service.getStudentHouseById('12345-123-12').streetAndNmr).toEqual(
+      'Lovensdijkstraat 100'
+    );
+    expect(service.getStudentHouseById('12345-123-12').streetAndNmr).not.toEqual(
+      'Lovensdijkstraat 62'
+    );
+    done();
+  });
+
+  it('should delete a studentHouse', (done: DoneFn) => {
+    service.deleteStudentHouse('12345-123-18');
+    expect(service.getStudentHouseById('12345-123-18')).toBeUndefined();
+    done();
+  });
+});
