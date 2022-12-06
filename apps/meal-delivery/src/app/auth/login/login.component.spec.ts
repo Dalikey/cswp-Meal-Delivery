85a9bcae215d2217e3e2ccd1255759d12052d509
Junhao Chen <junhao@hotmail.com>
2022-12-06 11:49:57 +0100

    NullInjectorError: No provider for CustomConfig!
    Agane

--- apps/meal-delivery/src/app/auth/login/login.component.spec.ts
+++ apps/meal-delivery/src/app/auth/login/login.component.spec.ts
@@ -4,7 +4,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { environment } from '../../../environments/environment';
-
+import { ConfigModule } from '../../shared/moduleconfig/config.module';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -15,7 +15,12 @@ describe('LoginComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
       schemas: [NO_ERRORS_SCHEMA],
-      imports: [RouterTestingModule, FormsModule, HttpClientModule],
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        HttpClientModule,
+        ConfigModule.forRoot({ apiEndpoint: environment.SERVER_API_URL }),
+      ],
     }).compileComponents();
   });
 
