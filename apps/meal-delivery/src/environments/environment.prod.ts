85b273c7b487980db62a3c106ad4c14303458461
Junhao Chen <junhao@hotmail.com>
2022-12-08 15:01:12 +0100

    Railway is 10000x better than Azure and Netlify is <3

--- apps/meal-delivery/src/environments/environment.prod.ts
+++ apps/meal-delivery/src/environments/environment.prod.ts
@@ -3,10 +3,8 @@
 export const environment = {
   production: true,
 
-  SERVER_DOCS_URL:
-    'https://apiwontdeploybecausemonorepopackagescannotberesolved.com/',
-  SERVER_API_URL:
-    'https://apiwontdeploybecausemonorepopackagescannotberesolved.com/',
+  SERVER_DOCS_URL: 'https://meal-delivery-api.up.railway.app/',
+  SERVER_API_URL: 'https://meal-delivery-api.up.railway.app/',
 
   appVersion: '1.2.3',
 };
