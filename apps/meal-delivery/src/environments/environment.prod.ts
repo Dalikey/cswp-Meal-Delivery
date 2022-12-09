fcdcff415598994269228fd2e4a0b2b8761ceb99
Junhao Chen <junhao@hotmail.com>
2022-12-09 19:17:35 +0100

    Stupid cors :/

--- apps/meal-delivery/src/environments/environment.prod.ts
+++ apps/meal-delivery/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@
 export const environment = {
   production: true,
 
-  SERVER_DOCS_URL: 'http://meal-delivery-api.up.railway.app/',
-  SERVER_API_URL: 'http://meal-delivery-api.up.railway.app/',
+  SERVER_DOCS_URL: 'https://meal-delivery-api.up.railway.app/',
+  SERVER_API_URL: 'https://meal-delivery-api.up.railway.app/',
 
   appVersion: '1.2.3',
 };
