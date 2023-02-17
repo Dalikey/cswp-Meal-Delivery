c89ce1cdcc847c6dec5be57f55e0f50c7d8430f6
Junhao Chen <junhao@hotmail.com>
2023-02-17 12:25:14 +0100

    Fixed neo4j but not using it because annoying

--- apps/meal-delivery-api/src/app/app.module.ts
+++ apps/meal-delivery-api/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { Neo4jModule } from './neo4j/neo4j.module';
       // 'mongodb://127.0.0.1:27017/api' // LocalDb
     ),
     Neo4jModule.forRoot({
-      scheme: 'neo4j+s',
+      scheme: 'neo4j',
       host: `${process.env.NEO4J_HOST}`,
       username: `${process.env.NEO4J_USR}`,
       password: `${process.env.NEO4J_PWD}`,
