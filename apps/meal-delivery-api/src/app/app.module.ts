2db60ced93ccf672d2c7b38192627373b5215800
Junhao Chen <junhao@hotmail.com>
2023-03-27 23:07:49 +0200

    Fix meal and locally for now

--- apps/meal-delivery-api/src/app/app.module.ts
+++ apps/meal-delivery-api/src/app/app.module.ts
@@ -11,8 +11,8 @@ import { DataModule } from './data.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
-      // 'mongodb://127.0.0.1:27017/api' // LocalDb
+      // `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+      'mongodb://127.0.0.1:27017/mealdelivery' // LocalDb the /mealdelivery is the name of the database
     ),
     AuthModule,
     DataModule,
--- apps/meal-delivery-api/src/app/meal/meal.service.ts
+++ apps/meal-delivery-api/src/app/meal/meal.service.ts
@@ -19,10 +19,13 @@ export class MealService {
   ) {}
 
   async createMeal(mealInfo: MealInfo, ownerId: string): Promise<ResourceId> {
-    const owner = await this.userModel.findOne({ id: ownerId });
+    const owner = await this.userModel.findOne({ id: ownerId }).select('_id');
 
     if (!owner) {
-      throw new HttpException('Owner not found', HttpStatus.BAD_REQUEST);
+      throw new HttpException(
+        `Owner with ID ${ownerId} not found`,
+        HttpStatus.BAD_REQUEST
+      );
     }
     const studentHouse = await this.studentHouseModel.findOne({
       id: mealInfo.studentHouseId,
@@ -41,11 +44,25 @@ export class MealService {
   }
 
   async getAll(): Promise<MealInfo[]> {
-    return this.mealModel.find({}, { _id: 0, __v: 0 });
+    return this.mealModel
+      .find({}, { _id: 0, __v: 0 })
+      .populate('ownerRef', { _id: 0, __v: 0 });
   }
 
   async getOne(mealId: string): Promise<MealInfo> {
-    const meals = await this.mealModel.aggregate([{ $match: { id: mealId } }]);
+    const meals = await this.mealModel.aggregate([
+      { $match: { id: mealId } },
+      { $project: { _id: 0, __v: 0 } },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'ownerRef',
+          foreignField: '_id',
+          as: 'owner',
+        },
+      },
+      { $project: { owner: { _id: 0, __v: 0 } } },
+    ]);
     return meals[0];
   }
 
@@ -62,62 +79,59 @@ export class MealService {
     if (!owner) {
       throw new HttpException('Owner not found', HttpStatus.BAD_REQUEST);
     }
-
-    if (owner.role !== 'admin') {
-      if (ownerId !== meal?.ownerRef?.id) {
-        throw new HttpException(
-          'You are not the owner of this meal.',
-          HttpStatus.BAD_REQUEST
-        );
-      }
+    if (owner.role !== 'admin' && ownerId !== meal?.ownerRef?.id) {
+      throw new HttpException(
+        'You are not the owner of this meal.',
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    if (!meal) {
+      throw new HttpException('Meal does not exist', HttpStatus.BAD_REQUEST);
     }
 
-    if (meal) {
-      try {
-        await this.mealModel.updateOne({ id: mealId }, [
-          {
-            $set: {
-              id: mealId,
-              name: mealInfo.name,
-              price: mealInfo.price,
-              deliveryTime: mealInfo.deliveryTime,
-              deliveryDate: mealInfo.deliveryDate,
-              owner: mealInfo.owner,
-            },
+    try {
+      const { id, ownerRef } = meal;
+      await this.mealModel.updateOne(
+        { id: mealId },
+        {
+          $set: {
+            name: mealInfo.name,
+            price: mealInfo.price,
+            deliveryTime: mealInfo.deliveryTime,
+            deliveryDate: mealInfo.deliveryDate,
+            owner: mealInfo.owner,
+            ownerRef: ownerRef,
           },
-        ]);
-      } catch (e) {
-        let errorMessage = 'Failed to do something exceptional';
-        if (e instanceof Error) {
-          errorMessage = e.message;
         }
-        throw new HttpException(errorMessage, HttpStatus.BAD_REQUEST);
+      );
+    } catch (e) {
+      let errorMessage = 'Failed to do something exceptional';
+      if (e instanceof Error) {
+        errorMessage = e.message;
       }
-    } else {
-      throw new HttpException('Meal does not exist', HttpStatus.BAD_REQUEST);
+      throw new HttpException(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
     return mealInfo;
   }
 
   async deleteOne(mealId: string, ownerId: string) {
-    const meal = await this.getOne(mealId);
-    const owner = await this.userModel.findOne({ id: ownerId });
+    const { owner, ...meal } = await this.getOne(mealId);
+    const foundOwner = await this.userModel.findOne({ id: ownerId });
 
-    if (meal) {
-      if (!owner) {
-        throw new HttpException('Owner not found', HttpStatus.BAD_REQUEST);
-      }
-      if (ownerId !== meal?.owner?.id) {
-        throw new HttpException(
-          'You are not the owner of this meal.',
-          HttpStatus.BAD_REQUEST
-        );
-      }
-
-      await this.mealModel.deleteOne({ id: mealId });
-    } else {
+    if (!meal) {
       throw new HttpException('Meal does not exist', HttpStatus.BAD_REQUEST);
     }
+    if (!foundOwner) {
+      throw new HttpException('Owner not found', HttpStatus.BAD_REQUEST);
+    }
+    if (owner![0].id !== ownerId) {
+      throw new HttpException(
+        'You are not the owner of this meal.',
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    await this.mealModel.deleteOne({ id: mealId });
   }
 }
