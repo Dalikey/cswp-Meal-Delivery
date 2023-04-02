381aa0527a065c6bf817a2fae535873b6d554f79
Junhao Chen <junhao@hotmail.com>
2023-04-02 21:21:38 +0200

    Fix path

--- apps/meal-delivery-api/src/app/user/user.service.ts
+++ apps/meal-delivery-api/src/app/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserInfo } from '@md/data';
-import { User as UserModel, UserDocument } from './user.schema';
+import { User as UserModel, UserDocument } from '../schema/user.schema';
 import { Neo4jService } from '../neo4j/neo4j.service';
 import { Identity, IdentityDocument } from '../schema/identity.schema';
 
