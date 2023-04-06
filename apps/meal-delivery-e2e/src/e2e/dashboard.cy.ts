020648006e633205d90f9f718bf447782fa84326
Junhao Chen <junhao@hotmail.com>
2023-04-06 22:03:09 +0200

    Man I need to fix a lot probably now im doing this

--- apps/meal-delivery-e2e/src/e2e/dashboard.cy.ts
+++ apps/meal-delivery-e2e/src/e2e/dashboard.cy.ts
@@ -10,4 +10,16 @@ describe('meal-delivery', () => {
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welkom bij Gouden maaltijden');
   });
+
+  it('should display big buttons', () => {
+    cy.get('h5').contains('Inloggen');
+    cy.get('h5').contains('Over ons');
+  });
+
+  it('should display a navbar', () => {
+    cy.get('.nav-link').contains('Home');
+    cy.get('.nav-link').contains('Over ons');
+    cy.get('.nav-link').contains('Registreer');
+    cy.get('.nav-link').contains('Login');
+  });
 });
