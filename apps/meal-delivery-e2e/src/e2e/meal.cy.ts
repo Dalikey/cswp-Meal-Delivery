86b9f201d3e14479c4841f840864b883261ea1d3
Junhao Chen <junhao@hotmail.com>
2023-04-07 17:58:43 +0200

    Complete meal crud

--- apps/meal-delivery-e2e/src/e2e/meal.cy.ts
+++ apps/meal-delivery-e2e/src/e2e/meal.cy.ts
@@ -10,7 +10,7 @@ describe('meals', () => {
 
     cy.contains('button', 'Inloggen').click();
   });
-
+  // --------------------------------------------------------------------------
   it('should create meal', () => {
     cy.get('.meal-card').click();
 
@@ -31,5 +31,51 @@ describe('meals', () => {
     cy.get('#studentHouse').select('Lovensdijkstraat 61', { force: true });
 
     cy.get('#save-meal-btn').click({ force: true });
+  });
+  // --------------------------------------------------------------------------
+  it('it should be able to see meals and products and their details', () => {
+    cy.get('.meal-card').click();
+
+    cy.get('a[href="/meal/59274d6a-6a27-4581-8890-3a4fb30f0b43"]').click({
+      force: true,
+    });
+
+    cy.get(
+      ':nth-child(1) > .card > .card-body > .no-bullets > :nth-child(3) > a'
+    ).click();
+  });
+  // --------------------------------------------------------------------------
+  it('should be able to edit a meal', () => {
+    cy.get('.meal-card').click();
+
+    cy.get(
+      ':nth-child(7) > .card > .card-body > .alignEnd > .edit > .material-icons'
+    ).click();
+
+    cy.get('#name').clear({ force: true }).type('Cypress bewerkte maaltijd');
+
+    cy.get('#price').clear({ force: true }).type('9.49');
+
+    cy.get('#deliverytime').click({ force: true });
+
+    cy.get('#deliverytime').type('12:00');
+
+    cy.get('#deliverydate').click({ force: true });
+
+    cy.get('#deliverydate').type('2023-04-07');
+
+    cy.get('#studentHouse').select('Lovensdijkstraat 61', { force: true });
+
+    cy.get('#save-meal-btn').click({ force: true });
+  });
+  // --------------------------------------------------------------------------
+  it('should be able to delete a meal', () => {
+    cy.get('.meal-card').click();
+
+    cy.get(
+      '.col-sm-12:nth-child(7) > .card > .card-body > .alignEnd > .delete > .material-icons'
+    ).click();
+
+    cy.get('.btn-danger').click();
   });
 });
